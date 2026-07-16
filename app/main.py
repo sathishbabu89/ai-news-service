084e0b54d1c607f2b81178bd609c896a52addf97
Sathish Kumar B <1370141@tcsgenailab.onmicrosoft.com>
2026-07-16 17:03:15 +0000

    Enable CORS for AskGenAI

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 
 from app.routers.articles import router as article_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="AI News Service",
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://askgenai.in",
+        "http://localhost:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
